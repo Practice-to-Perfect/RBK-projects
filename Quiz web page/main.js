28fc7a118e282d0cb4c8dea9f924e6af39f28aac
saraalarajabii <saraalrajabii12@gmail.com>
2020-10-05 09:50:15 +0300

    gif-edit

--- Quiz web page/main.js
+++ Quiz web page/main.js
@@ -21,7 +21,7 @@ function marks() {
 		trueValues++;
 	}
 var notes=["Hmmmm!","Okay we can work with this..","Great! You can do more..","Wow! You're well educated!"];
-var images=["imge/1.gif","imge/2.gif","imge/3.gif","imge/4.gif"];
+var images=["imge/1.gif","imge/2.gif","imge/3.gif","imge/44.gif"];
 
 if (document.getElementById("Q").value != "") {
  if (trueValues==0||trueValues==1){
